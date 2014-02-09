ca294c9d29cbd879f60e3c5bfd83bb1883cff51e
Ralph Schleicher <rs@ralph-schleicher.de>
2014-02-09 09:35:22 +0000

    (make-cie-luv-color): Fix white point argument.

--- cie-luv.lisp
+++ cie-luv.lisp
@@ -66,9 +66,9 @@
   (slot-value color 'white-point))
 
 (export 'make-cie-luv-color)
-(defun make-cie-luv-color (L* u* v* &optional (white-point CIE-1931-D50))
+(defun make-cie-luv-color (L* u* v* &optional white-point)
   "Create a new color in the CIE L*u*v* color space."
-  (make-instance 'cie-luv-color :L* (ensure-type L* '(real 0)) :u* (ensure-type u* 'real) :v* (ensure-type v* 'real) :white-point (ensure-type white-point 'color-object)))
+  (make-instance 'cie-luv-color :L* (ensure-type L* '(real 0)) :u* (ensure-type u* 'real) :v* (ensure-type v* 'real) :white-point (ensure-type  (or white-point CIE-1931-D50) 'color-object)))
 
 (defun cie-uv-from-xy (x y s)
   (declare (type real x y s))
